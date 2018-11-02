b11c7160413c167402d5f545000e7a764dedd200
IMH51 <ianmartinharrison@outlook.com>
2018-11-02 12:09:15 +0000

    all tests pass with reorganized pigeon hash

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  reorganized_pigeons = {}
+  data.each do |attribute, values|
+    values.each do |value, names|
+      names.each do |name|
+        reorganized_pigeons[name] = {}
+      end
+    end
+  end
+  data.each do |attribute, values|
+    reorganized_pigeons.each do |name, stats|
+      reorganized_pigeons[name][attribute] = []
+    end
+  end
+  data.each do |attribute, values|
+    values.each do |value, names|
+      reorganized_pigeons.each do |name, stats|
+        if names.include?(name)
+          reorganized_pigeons[name][attribute] << value.to_s
+        end
+      end
+    end
+  end
+  reorganized_pigeons
 end
