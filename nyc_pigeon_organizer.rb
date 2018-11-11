ee0115d93b431c678ede54fdf8bf7733ac517aa4
Nathan Bell <nathanbell09@gmail.com>
2018-11-11 17:51:10 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
     value.each do |info, pigeons|
       pigeons.each do |name|
         if pigeon_list.include?(name)
-          if pigeon_list[name].include?(attribute) && !pigeon_list[name][attribute].include?(info.to_s)
+          if pigeon_list[name].include?(attribute)
             pigeon_list[name][attribute] << info.to_s
           elsif !pigeon_list[name].include?(attribute)
             pigeon_list[name][attribute] = [info.to_s]
