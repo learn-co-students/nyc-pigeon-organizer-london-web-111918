e7dce04ec5e16bd48932b91440f61799cc4bd2fe
jodyneckles <jody@technative.tv>
2018-11-15 22:14:35 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(hash_data)
+  new_hash = {}
+  hash_data.each do |attr, data|
+  	data.each do |value, names|
+  		names.each do |name|
+  			if new_hash[name] == nil
+  				new_hash[name] = {}
+  				new_hash[name][attr] = []
+  			elsif new_hash[name][attr] == nil
+  				new_hash[name][attr] = []
+  			end
+  			new_hash[name][attr] << value.to_s
+  		end
+  	end
+  end
+  new_hash
 end
